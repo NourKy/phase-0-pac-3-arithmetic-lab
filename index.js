d66078d1f3fefe5a3fe9555da09eab3f822e63d6
Nour kayyali <nourkayyali91@gmail.com>
2023-10-02 00:26:31 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,69 @@
+let number = 5;
+let result;
+function add()
+{
+
+}
+add();
+subtract();
+function subtract()
+{
+
+}
+multiply();
+function multiply()
+{
+
+}
+divide();
+function divide(){
+
+}
+add(a, b);
+function add(a, b)
+{
+     result=a+b;
+    return result
+}
+subtract(a, b) ;
+function  subtract(a, b) 
+{
+     result= a-b;
+    return result;
+}
+multiply(a, b) ;
+function multiply(a, b) 
+{
+    result= a*b;
+    return result;
+}
+divide(a, b);
+function  divide(a, b)
+{
+    result= a/b;
+    return result;
+}
+increment(n);
+function increment(n)
+{
+result=n+1;
+return result;
+}
+decrement(n);
+function decrement(n)
+{
+    result=n-1;
+    return result;
+}
+makeInt(n);
+function makeInt(n)
+{
+    result=parseInt(n,10);
+    return result;
+}
+preserveDecimal(n);
+function preserveDecimal(n)
+{
+    result=parseFloat(n);
+    return result;
+}
